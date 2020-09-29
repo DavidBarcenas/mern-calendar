de5e86bbfc91c3135ee45b28a4da74e8b7e35717
David Barcenas <daveepro@outlook.com>
2020-09-29 16:34:46 -0500

    feat: Add react big calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 import { Navbar } from '../ui/Navbar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
 
-export const CalendarScreen = () => {
+const localizer = momentLocalizer(moment)
+
+const myEventsList = []
+
+export const CalendarScreen = props => {
   return (
-    <div>
+    <div className="calendar__wrapper">
       <Navbar />
+
+      <div>
+        <Calendar
+          localizer={localizer}
+          events={myEventsList}
+          startAccessor="start"
+          endAccessor="end"
+        />
+      </div>
     </div>
   )
 }
