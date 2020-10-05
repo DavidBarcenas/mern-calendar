ddc9c561488aefc01e958024d379aeb8d4536e17
David Barcenas <daveepro@outlook.com>
2020-10-05 11:32:21 -0500

    Move files to front end

--- src/redux/types/types.js
+++
@@ -1,8 +0,0 @@
-export const types = {
-  uiOpenModal: '[ui] Open modal',
-  uiCloseModal: '[ui] Close modal',
-  eventSetActive: '[event] Set active',
-  addNewEvent: '[event] Add event',
-  updateEvent: '[event] Update event',
-  deleteEvent: '[event] Delete event'
-}
