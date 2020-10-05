4d84d001e2bfb62e1e14eb7fb0d1c8b4494361bf
David Barcenas <daveepro@outlook.com>
2020-10-05 11:29:26 -0500

    refactor: Remove blanks and create folder for front and back

---
+++ frontend/src/redux/types/types.js
@@ -0,0 +1,8 @@
+export const types = {
+  uiOpenModal: '[ui] Open modal',
+  uiCloseModal: '[ui] Close modal',
+  eventSetActive: '[event] Set active',
+  addNewEvent: '[event] Add event',
+  updateEvent: '[event] Update event',
+  deleteEvent: '[event] Delete event'
+}
