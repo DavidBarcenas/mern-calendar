924c380a7603e64cc89971dc76770e9d4c2d3743
David Barcenas <daveepro@outlook.com>
2020-10-05 17:39:29 -0500

    backend: Add express, configure port and create public folder

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
-console.log('hello world!')
+const express = require('express')
+require('dotenv').config()
+
+// Create express server
+const app = express()
+
+// Public folder
+app.use(express.static('public'))
+
+// listen port
+app.listen(process.env.PORT, () => {
+  console.log('Run server in port', process.env.PORT)
+})
