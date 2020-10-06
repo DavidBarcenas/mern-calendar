144657a3d4a56f06b0b8c63f9a1bc1150abdf75f
David Barcenas <daveepro@outlook.com>
2020-10-05 22:26:21 -0500

    backend(auth): Generate JWT [jsonwebtoken]

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const bcrypt = require('bcryptjs')
 const User = require('../models/User')
+const { generateJWT } = require('../utils/jwt')
 
 const createUser = async (req, res = response) => {
   const {email, pwd} = req.body
@@ -19,11 +20,13 @@ const createUser = async (req, res = response) => {
     user.pwd = bcrypt.hashSync(pwd, salt)
 
     await user.save()
+    const token = await generateJWT(user.id, user.name)
 
     res.status(201).json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     })
   } catch (error) {
     res.status(500).json({
@@ -47,6 +50,7 @@ const loginUser = async (req, res = response) => {
     }
 
     const validPwd = bcrypt.compareSync(pwd, user.pwd)
+
     if(!validPwd) {
       return res.status(400).json({
         ok: false,
@@ -54,10 +58,14 @@ const loginUser = async (req, res = response) => {
       })
     }
 
+    // JWT
+    const token = await generateJWT(user.id, user.name)
+
     res.status(200).json({
       ok: true,
       uid: user.id,
-      name: user.name
+      name: user.name,
+      token
     })
     
   } catch (error) {
