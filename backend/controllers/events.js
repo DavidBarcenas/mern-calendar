36ed22b51c38163faf73cb5bbd654a226f003143
David Barcenas <daveepro@outlook.com>
2020-10-06 17:23:58 -0500

    backend(events): Create event endpoints

---
+++ backend/controllers/events.js
@@ -0,0 +1,36 @@
+
+const getEvents   = (req, res, next) => {
+  return res.json({
+    ok: true,
+    msg: 'GetEvents'
+  })
+}
+
+const createEvent = (req, res, next) => {
+  return res.json({
+    ok: true,
+    msg: 'createEvent'
+  })
+}
+
+const updateEvent = (req, res, next) => {
+  return res.json({
+    ok: true,
+    msg: 'updateEvent'
+  })
+}
+
+const deleteEvent = (req, res, next) => {
+  return res.json({
+    ok: true,
+    msg: 'deleteEvent'
+  })
+}
+
+
+module.exports = {
+  getEvents,
+  createEvent,
+  updateEvent,
+  deleteEvent
+}
---
+++ backend/routes/events.js
@@ -0,0 +1,19 @@
+/**
+ * User Routes / Events
+ * host + /api/events
+ */
+
+const express = require('express')
+const { getEvents, createEvent, updateEvent, deleteEvent } = require('../controllers/events')
+const { validateJWT } = require('../middlewares/validate-jwt')
+
+
+const router = express.Router()
+router.use(validateJWT)
+
+router.get('/', getEvents)
+router.post('/', createEvent)
+router.put('/:id', updateEvent)
+router.delete('/:id', deleteEvent)
+
+module.exports = router;
