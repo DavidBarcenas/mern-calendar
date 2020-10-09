3847d1437494e7deb0575fd21690ab3361376eed
David Barcenas <daveepro@outlook.com>
2020-10-09 09:16:18 -0500

    test: StartLogin action [localStorage, store]

---
+++ frontend/src/tests/actions/auth.test.js
@@ -0,0 +1,39 @@
+import '@testing-library/jest-dom';
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk';
+import { startLogin } from '../../redux/actions/auth';
+import { types } from '../../redux/types/types'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+const initState = {}
+let store = mockStore(initState)
+
+Storage.prototype.setItem = jest.fn()
+
+describe('actions testing', () => {
+  beforeEach(() => {
+    store = mockStore(initState)
+    jest.clearAllMocks()
+  })
+
+  test('it should save the token and trigger the login', async () => {
+    await store.dispatch(startLogin('davee@gmail.com', 'dave123'))
+    const actions = store.getActions()
+
+    expect(actions[0]).toEqual({
+      type: types.authLogin,
+      payload: {
+        uid: expect.any(String),
+        name: expect.any(String)
+      }
+    })
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('token', expect.any(String))
+    expect(localStorage.setItem).toHaveBeenCalledWith('token-init', expect.any(Number))
+
+    // token = localStorage.setItem.mock.calls[0][1]
+  })
+    
+})
